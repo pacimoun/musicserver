ab2d6dd35dbf370fb771ca63eed1f8c4e04682c8
Edward Loginov <tamerlan701@gmail.com>
2026-05-06 17:43:53 +0700

    added verbose to download_sc

--- syncworker/scripts/download_sc.sh
+++ syncworker/scripts/download_sc.sh
@@ -2,7 +2,7 @@
 source "$(dirname "$0")/config.sh"
 
 echo "--- Шаг 1: Загрузка новых треков из SoundCloud ---"
-yt-dlp -i --download-archive "$ARCHIVE_FILE" \
+yt-dlp -v -i --download-archive "$ARCHIVE_FILE" \
   --format "bestaudio/best" --extract-audio --audio-quality 0 \
   --embed-metadata --embed-thumbnail \
   --output "$MUSIC_DIR/[%(id)s] %(title)s.%(ext)s" \
